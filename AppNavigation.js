f65e6984c51ce382610adc0e42b7df44cf179252
Pranay <pranay@Pranays-MacBook-Air.local>
2025-03-22 20:05:59 +0530

    added stack

--- AppNavigation.js
+++ AppNavigation.js
@@ -1,27 +1,25 @@
 import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 
-
+// Import Screens
 import HomeScreen from './screens/Home';
 import ProfileScreen from './screens/Profile';
 import SettingsScreen from './screens/Settings';
 import FirebaseMessagingScreen from './screens/FirebaseMessagingScreen';
 
-const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
 
 const AppNavigation = () => {
     return (
         <NavigationContainer>
-            <Tab.Navigator>
-                <Tab.Screen name="Hell" component={FirebaseMessagingScreen} />
-
-                <Tab.Screen name="Home" component={HomeScreen} />
-                <Tab.Screen name="Profile" component={ProfileScreen} />
-                <Tab.Screen name="Settings" component={SettingsScreen} />
-
-            </Tab.Navigator>
-        </NavigationContainer >
+            <Stack.Navigator>
+                <Stack.Screen name="Firebase" component={FirebaseMessagingScreen} />
+                <Stack.Screen name="Home" component={HomeScreen} />
+                <Stack.Screen name="Profile" component={ProfileScreen} />
+                <Stack.Screen name="Settings" component={SettingsScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
     );
 };
 
