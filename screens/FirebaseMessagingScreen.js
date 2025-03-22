5eda19f358182cfdf54e8a94c014727c1b820ae9
Pranay <pranay@Pranays-MacBook-Air.local>
2025-03-22 20:07:26 +0530

    added stack

--- screens/FirebaseMessagingScreen.js
+++ screens/FirebaseMessagingScreen.js
@@ -1,12 +1,25 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Button } from 'react-native';
+import React, { useEffect } from 'react';
+import { useNavigation } from '@react-navigation/native';
 
 const FirebaseMessagingScreen = () => {
+    const navigation = useNavigation();
+
+    // Redirect to Home after 3 seconds
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            navigation.replace('Home'); // Replace so the user can't go back
+        }, 3000);
+
+        return () => clearTimeout(timer); // Cleanup on unmount
+    }, []);
+
     return (
         <View>
             <Text>FirebaseMessagingScreen</Text>
+            <Button title="Go to Home Now" onPress={() => navigation.replace('Home')} />
         </View>
-    )
-}
+    );
+};
 
-export default FirebaseMessagingScreen
+export default FirebaseMessagingScreen;
